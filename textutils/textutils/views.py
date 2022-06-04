2e465cc43b7ba08416ce1bdb205f5271a32dc658
Archak <profile.archak@gmail.com>
2022-06-04 10:15:54 +0545

    Tutorial 12 (Extra Space Remover, New line remover, Character Counter etc. features added)

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -13,17 +13,72 @@ def analyze(request):
     #getting text from text field and printing it in Remove Punctuation Page
     displayText=request.GET.get('text', 'default')
     #getting checkbox input
-    displayPunc=request.GET.get('removePunc', 'off')
+    removePunc=request.GET.get('removePunc', 'off')
+    uppercase=request.GET.get('uppercase', 'off')
+    newlineremover=request.GET.get('newlineremover', 'off')
+    extraspaceremover=request.GET.get('extraspaceremover', 'off')
+    charactercounter=request.GET.get('charactercounter', 'off')
+
     #removing punctuations in user entered text
-    if displayPunc == "on":
+    if (removePunc == "on"):
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
+        #Checking if any characters is a punctuation character
         for char in displayText:
             if char not in punctuations:
                 analyzed = analyzed + char
+        #storing parameters in Json form
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        # rendering results in template
         return render(request, 'analyze.html', params)
 
+    # Changing user entered characters into uppercase
+    elif (uppercase=="on"):
+        analyzed = ""
+        # iterating through every letters, capitalizing them and merging them
+        for char in displayText:
+            analyzed = analyzed + char.upper()
+        # storin results in Json form
+        params = {'purpose': 'Changed to UpperCase', 'analyzed_text': analyzed}
+        # rendering results in template
+        return render(request, 'analyze.html', params)
+
+    # Removing new line in given text
+    elif (newlineremover=="on"):
+        analyzed = ""
+        # iterating through every letters, removing new lines and merging them
+        for char in displayText:
+            if char != "\n":
+                analyzed = analyzed + char
+        # storin results in Json form
+        params = {'purpose': 'New Line Removed', 'analyzed_text': analyzed}
+        # rendering results in template
+        return render(request, 'analyze.html', params)
+
+    # Removing extra spaces from given texts
+    elif (extraspaceremover=="on"):
+        analyzed = ""
+        # iterating through every characters, removing extra spaces and merging them
+        for index, char in enumerate(displayText):
+            if not(displayText[index] == " " and displayText[index+1] == " "):
+                analyzed = analyzed + char
+        # storin results in Json form
+        params = {'purpose': 'Extra Space Removed', 'analyzed_text': analyzed}
+        # rendering results in template
+        return render(request, 'analyze.html', params)
+
+    
+    # Counting Number of characters in given text
+    elif (charactercounter=="on"):
+        count = 0
+        # iterating through every characters, removing extra spaces and merging them
+        for char in displayText:
+            count = count +1
+        # storin results in Json form
+        params = {'purpose': 'Character Counted', 'analyzed_text': count}
+        # rendering results in template
+        return render(request, 'analyze.html', params)
+
+    # results to show if no punctuation is choosed
     else:
         return HttpResponse('Error')
-    return HttpResponse("Remove Punctuation <a href='/'> Back</a>")
